934fe9dce30d0afafbdf1c10ab8223dc21113683
Derek Hearn <derek@meetball.com>
2015-05-29 15:01:22 -0500

    Initial Commit

---
+++ StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StringExtensions
+{
+	public static class StringExtensions
+	{
+		public static string maxLengthTrim(this String str, int maxLength)
+		{
+			if (str == null)
+				return str;
+			if (str.Length > maxLength)
+				return str.Substring(0, maxLength - 1);
+			return str;
+		}
+
+		public static bool isNullOrEmpty(this String str)
+		{
+			return String.IsNullOrEmpty(str);
+		}
+
+		public static bool isNotNullOrEmpty(this String str)
+		{
+			return !String.IsNullOrEmpty(str);
+		}
+
+		public static bool isNullOrWhiteSpace(this String str)
+		{
+			return String.IsNullOrWhiteSpace(str);
+		}
+
+		public static bool isNotNullOrWhiteSpace(this String str)
+		{
+			return !String.IsNullOrWhiteSpace(str);
+		}
+	}
+}
